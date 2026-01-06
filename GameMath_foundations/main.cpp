9d6a9539408c35557f374d2dea4112c54a6b3364
darren29221309 <darren29221309@gmail.com>
2026-01-07 00:11:42 +0800

    [Math] Implement Matrix4x4 multiplication and pass translation test

--- GameMath_foundations/main.cpp
+++ GameMath_foundations/main.cpp
@@ -7,6 +7,8 @@ const float PI = 3.14159265359f;
 struct Vector3 {
 	float X,Y,Z;
 
+
+
 	float Dot(const Vector3& Other) const {
 		return (X * Other.X) + (Y * Other.Y) + (Z * Other.Z);
 	}
@@ -33,22 +35,50 @@ struct Vector3 {
 
 };
 
+struct Matrix4x4 {
+	float m[4][4];
+
+	Matrix4x4() {
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				m[i][j] = (i == j) ? 1.0f : 0.0f;
+			}
+		}
+	}
+
+	Matrix4x4 operator*(const Matrix4x4& other) const {
+		Matrix4x4 result;
+		for (int row = 0; row < 4; row++) {
+			for (int col = 0; col < 4; col++) {
+				float sum = 0.0f;
+				for (int k = 0; k < 4; k++) {
+					sum += m[row][k] * other.m[k][col];
+				}
+				result.m[row][col] = sum;
+			}
+		}
+		return result;
+	}
+
+};
 
 int main() {
-	float degree = 330.0f;
-	float radian = degree * (PI / 180.0f);
+	Matrix4x4 translationMatrix;
 
-	Vector3 EnemyForward = { 1.0, 0.0f, 0.0f };
-	
-	Vector3 DirToPlayer = {
-		std::cos(radian),
-		std::sin(radian),
-		0.0f
-	};
+	translationMatrix.m[3][0] = 10.0f;
+	translationMatrix.m[3][1] = 0.0f;
+	translationMatrix.m[3][2] = 0.0f;
 
-	Vector3 crossResult = EnemyForward.Cross(DirToPlayer);
+	Matrix4x4 translationMatrix2;
 
-	std::cout <<"cross result(z-axis): " << crossResult.Z << std::endl;
+	translationMatrix2.m[3][1] = 5.0f;
+
+	Matrix4x4 combineMatrix = translationMatrix * translationMatrix2;
+
+	std::cout << "combined Translation: ("
+		<< combineMatrix.m[3][0] << ", "
+		<< combineMatrix.m[3][1] << ", "
+		<< combineMatrix.m[3][2] << ")" << std::endl;
 
 	
 	std::system("pause");
