f2c86ec7d1efbd6d9ac97b8cfdee458b3b31c188
darren <darren@fs-technology.com>
2026-01-06 15:34:48 +0800

    [Math] Complete Dot and Cross product validation experiments

--- GameMath_foundations/main.cpp
+++ GameMath_foundations/main.cpp
@@ -1,15 +1,56 @@
 #include <iostream>
+#include <cmath>
+#include <iomanip>
 
-class Player {
-	double Health;
-	int Score;
-	bool IsAlive;
+const float PI = 3.14159265359f;
+
+struct Vector3 {
+	float X,Y,Z;
+
+	float Dot(const Vector3& Other) const {
+		return (X * Other.X) + (Y * Other.Y) + (Z * Other.Z);
+	}
+
+	Vector3 Cross(const Vector3& Other) const {
+		return Vector3{
+			Y * Other.Z - Z * Other.Y,
+			Z * Other.X - X * Other.Z,
+			X * Other.Y - Y * Other.X
+		};
+	}
+
+	float SizeSquared() const {
+		return X * X + Y * Y + Z * Z;
+	}
+
+	void Normalize() {
+		float Length = std::sqrt(SizeSquared());
+		if (Length > 0.00001f) {
+			X /= Length; Y /= Length; Z /= Length;
+		}
+	}
+
+
 };
 
+
 int main() {
-	std::cout << "player class size: " << sizeof(Player) << " bytes" << std::endl;
+	float degree = 330.0f;
+	float radian = degree * (PI / 180.0f);
 
+	Vector3 EnemyForward = { 1.0, 0.0f, 0.0f };
+	
+	Vector3 DirToPlayer = {
+		std::cos(radian),
+		std::sin(radian),
+		0.0f
+	};
 
+	Vector3 crossResult = EnemyForward.Cross(DirToPlayer);
+
+	std::cout <<"cross result(z-axis): " << crossResult.Z << std::endl;
+
+	
 	std::system("pause");
 	return 0;
 }
