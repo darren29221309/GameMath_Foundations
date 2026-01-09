3c2dd9995b4cc215ad901d72d632c5d184508b8f
darren <darren@fs-technology.com>
2026-01-09 16:06:35 +0800

    test

--- GameMath_foundations/main.cpp
+++ GameMath_foundations/main.cpp
@@ -7,15 +7,33 @@
 const float PI = 3.14159265359f;
 
 int main() {
+	Vector3 playerPos(0.5f, 0.0f, 0.0f);
+	Vector3 playerScale(1, 1, 1);
+
+	Vector3 WallPos(2.0f, 0.0f, 0.0f);
+	Vector3 WallScale(1, 1, 1);
+
+
 	AABB playerBox;
-	playerBox.Update(Vector3(0.5f,0.0f,0.0f),Vector3(1,1,1)); // 玩家半徑為 1
+	playerBox.Update(playerPos, playerScale); // 玩家半徑為 1
 
 	AABB wallBox;
-	wallBox.Update(Vector3(2, 0, 0), Vector3(1, 1, 1)); // 牆在 X=2 的位置
+	wallBox.Update(WallPos, WallScale); // 牆在 X=2 的位置
 
 	CollisionInfo info =  playerBox.GetCollisionInfo(wallBox);
 
 	printf("碰到:%s\nX:%f Y:%f Z:%f\n",info.bIsHit?"true":"false",info.OverlapX,info.OverlapY,info.OverlapZ);
+
+	if (info.bIsHit) {
+		float overlaps[3] = { info.OverlapX,info.OverlapY ,info.OverlapZ };
+
+		//找出最小距離軸
+		int minAxis = 0;
+		if (overlaps[1] < overlaps[minAxis])minAxis = 1;
+		if (overlaps[2] < overlaps[minAxis])minAxis = 2;
+
+
+	}
 
 	std::system("pause");
 	return 0;
