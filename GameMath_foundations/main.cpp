373abc4fa0fd32427990d363b4a39625478b07a5
darren29221309 <darren29221309@gmail.com>
2026-01-11 23:22:19 +0800

    Feature: Implement sliding collision logic and MTV position resolution

--- GameMath_foundations/main.cpp
+++ GameMath_foundations/main.cpp
@@ -10,7 +10,7 @@ int main() {
 	Vector3 playerPos(0.5f, 0.0f, 0.0f);
 	Vector3 playerScale(1, 1, 1);
 
-	Vector3 WallPos(2.0f, 0.0f, 0.0f);
+	Vector3 WallPos(3.0f, 0.0f, 0.0f);
 	Vector3 WallScale(1, 1, 1);
 
 
@@ -20,10 +20,14 @@ int main() {
 	AABB wallBox;
 	wallBox.Update(WallPos, WallScale); // 牆在 X=2 的位置
 
+
+	//假設這是玩家這一個影格想要移動的方向
+	Vector3 velocity(1.0f, 1.0f, 0.0f);
+
 	CollisionInfo info =  playerBox.GetCollisionInfo(wallBox);
 
-	printf("碰到:%s\nX:%f Y:%f Z:%f\n",info.bIsHit?"true":"false",info.OverlapX,info.OverlapY,info.OverlapZ);
 
+	
 	if (info.bIsHit) {
 		float overlaps[3] = { info.OverlapX,info.OverlapY ,info.OverlapZ };
 
@@ -32,9 +36,37 @@ int main() {
 		if (overlaps[1] < overlaps[minAxis])minAxis = 1;
 		if (overlaps[2] < overlaps[minAxis])minAxis = 2;
 
+		if (minAxis == 0) {
+			if (playerPos.X < WallPos.X) playerPos.X -= info.OverlapX;
+			else playerPos.X += info.OverlapX;
+
+			velocity.X = 0;
+			printf(">>> 撞擊 X 軸: 位置已修正，現改為沿 Y 軸滑動\n");
+		}
+		else if (minAxis == 1) {
+			if (playerPos.Y < WallPos.Y) playerPos.Y -= info.OverlapY;
+			else playerPos.Y += info.OverlapY;
+
+			velocity.Y = 0;
+			printf(">>> 撞擊 Y 軸: 位置已修正，現改為沿 X 軸滑動\n");
+		}
+		else {
+			if (playerPos.Z < WallPos.Z) playerPos.Z -= info.OverlapZ;
+			else playerPos.Z += info.OverlapZ;
+
+			velocity.Z = 0;
+			printf(">>> 撞擊 Z 軸：位置已修正\n");
+		}
+
+		playerBox.Update(playerPos, playerScale);
 
 	}
 
+	playerPos.X += velocity.X;
+	playerPos.Y += velocity.Y;
+	playerPos.Z += velocity.Z;
+
+	printf("最終玩家位置: (%.2f, %.2f, %.2f)\n", playerPos.X, playerPos.Y, playerPos.Z);
 	std::system("pause");
 	return 0;
 }
